ab3001ad14824acb6c3f8dc6542e8eedcaa01c0e
Jon Naske <jon@shorecel.com>
2023-10-05 16:37:43 -0700

    added check to make sure api is running

--- ShiftLoggerClient/Program.cs
+++ ShiftLoggerClient/Program.cs
@@ -9,5 +9,17 @@ var baseUrlResult = Uri.TryCreate(Environment.GetCommandLineArgs()[1],
     UriKind.Absolute, out _);
 if (!baseUrlResult)
     throw new ArgumentException("Please provide a valid url");
+try
+{
+    using var client = new HttpClient();
+    var response = client.GetAsync($"{args[0]}api/ping").Result;
+    if (!response.IsSuccessStatusCode)
+        throw new HttpRequestException();
+}
+catch (HttpRequestException)
+{
+    throw new HttpRequestException(
+        "Please provide a valid url, or make sure the api is running");
+}
 
 UserInterface.MainMenu();
